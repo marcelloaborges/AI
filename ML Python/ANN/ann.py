e74b6cd3a997d659bcee7b909857741c2a9afa27
marcello.borges <marcello.borges@hbsis.com.br>
2018-07-04 17:16:33 -0300

    changed the train/test set size

--- ML Python/ANN/ann.py
+++ ML Python/ANN/ann.py
@@ -25,7 +25,7 @@ x = onehotencoder.fit_transform(x).toarray()
 x = x[:, 1:]
 
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.1, random_state = 0)
 
 
 from sklearn.preprocessing import StandardScaler
